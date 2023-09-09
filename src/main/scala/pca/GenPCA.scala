855f4d1e88133d4446867da0a80afa7a2b1bacbb
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2023-09-09 13:45:47 -0500

    desiredname workaround for firtool

--- src/main/scala/pca/GenPCA.scala
+++ src/main/scala/pca/GenPCA.scala
@@ -6,11 +6,16 @@ import _root_.circt.stage.ChiselStage // for 5.0.0
 object GenPCA {
   def main(args: Array[String]): Unit = {
     // option handling
+    val n=256
+    val nbits_px=8
+    val nbits_iem=8
     val opts = Array("--disable-all-randomization",
        "--strip-debug-info",
-       "--split-verilog",
-       "-o=generated")
+      //   "--split-verilog", "-o=generated",
+      "--lowering-options=disallowLocalVariables",
+      "--verilog", s"-o=VMulRed_n${n}_px${nbits_px}_iem${nbits_iem}.v"
+    )
 
-    ChiselStage.emitSystemVerilog(new VMulRed(n=1024,nbits_px=8,nbits_iem=8), firtoolOpts = opts) // 5.0.0
+    ChiselStage.emitSystemVerilog(new VMulRed(n,nbits_px,nbits_iem), firtoolOpts = opts) // 5.0.0
   }
 }
