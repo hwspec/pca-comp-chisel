15a36b77702a32e6c86d1e7a9f3f878322e55860
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2024-12-26 22:36:05 -0600

    encmat loading

--- src/main/scala/pca/PCAComp.scala
+++ src/main/scala/pca/PCAComp.scala
@@ -10,15 +10,29 @@ class PCAComp(ninpixels: Int = 4096, pxbw: Int = 12, maxenc: Int = 30, encbw: In
     val out = Decoupled(Vec(maxenc, SInt(encbw.W)))
     //
     val loadenc = Input(Bool()) // load encdata into encmat at encpos
-    val encpos  = Input(UInt(log2Ceil(maxenc).W))
-    val encdata = Input(Vec(ninpixels, UInt(pxbw.W)))
+    val verifyenc = Input(Bool()) // load encdata into encmat at encpos
+    val encpos  = Input(UInt(log2Ceil(ninpixels).W))
+    val encdata = Input(Vec(maxenc, SInt(encbw.W)))
+    val encdataverify = Output(Vec(maxenc, SInt(encbw.W)))
   })
 
   io.in.ready := false.B
   io.out.valid := false.B
   for (i <- 0 until maxenc) io.out.bits(i) := 0.S
-}
 
+  val encmat = SyncReadMem(ninpixels, Vec(maxenc, SInt(encbw.W)))
+  when (io.loadenc) {
+    encmat(io.encpos) := io.encdata
+  }
+  when (io.verifyenc) {
+    io.encdataverify := encmat(io.encpos)
+  }.otherwise {
+    for (i <- 0 until maxenc) {
+      io.encdataverify(i) := 0.S
+    }
+  }
+
+}
 
 object PCAComp extends App {
   GenVerilog.generate(new PCAComp)
