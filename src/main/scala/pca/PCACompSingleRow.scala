4612cf39f92f91446f1c12f8ec07f9ebccaff39f
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2025-06-11 11:55:46 -0500

    crafting IO

--- src/main/scala/pca/PCACompSingleRow.scala
+++ src/main/scala/pca/PCACompSingleRow.scala
@@ -8,18 +8,22 @@ class PCACompSingleRow(
                         // pixel-sensor params. the width and height of a block
                         ncols: Int = 192, // the numbers of the pixel-sensor columns
                         nrows: Int = 168, // the numbers of the pixel-sensor rows
-                        pxbw: Int = 12,   // pixel bit width
-                        width: Int = 24,  // width for this block. ncols%widht == 0
-                        // PCA params
-                        npcs  : Int = 30, // the max number of principal components
+                        pxbw: Int = 12, // pixel bit width
+                        width: Int = 24, // width for this block. ncols%widht == 0
+                        // PCA params, iem=inverse encoding matrix
+                        nmaxpcs  : Int = 30, // the max number of principal components
                         iemfloat : Boolean = false, // false: signed integer
                         iembw    : Int = 8, // encoding bit width for int, mantissa bit for float
-                        iemexp   : Int = 0, // exponent bit for float
+                        iemexp   : Int = 0, // exponent bit for float. unused for signed integer
+                        // SRAM params.
+                        nsrams : Int = 4,  // the number of srams with the depth nrows
+                        // width * nmaxpcs * iembw / busbw
                         // other params
                         debugprint: Boolean = true
                       ) extends Module {
 
   require( (ncols%width)==0 )
+  require( iemfloat == false, "float is not supported yet")
 
   private val mulbw = pxbw + iembw    // internal use. signed
   val redbw = if (iemfloat) {
@@ -28,28 +32,28 @@ class PCACompSingleRow(
     1 + iemexp + iembw
   }
 
-  override def desiredName = s"BaseLinePCAComp_pxbw${pxbw}_w${width}_iembw${iembw}_npcs${npcs}"
+  override def desiredName = s"BaseLinePCAComp_pxbw${pxbw}_w${width}_iembw${iembw}_npcs${nmaxpcs}"
 
   val io = IO(new Bundle {
     // input : no stall condition for now
-    val npc     = Input(UInt(log2Ceil(npcs).W)) // the number of principal components used
+    val npc     = Input(UInt(log2Ceil(nmaxpcs).W)) // the number of principal components used
     val rowid   = Input(UInt(log2Ceil(nrows).W))
     val invalid = Input(Bool())
     val indata  = Input(Vec(width, UInt(pxbw.W)))
     // output (buffered)
-    val out  = Decoupled(Vec(npcs, SInt(redbw.W))) // compressed data
+    val out  = Decoupled(Vec(nmaxpcs, SInt(redbw.W))) // compressed data
 
     // initialize memory with the inverse encoding matrix content for this block
     val updateIEM     = Input(Bool()) // load imedata into mem
     val verifyIEM     = Input(Bool()) // read mem for verification
     val rowpos        = Input(UInt(log2Ceil(nrows).W))
-    val iempos        = Input(UInt(log2Ceil(npcs).W))
+    val iempos        = Input(UInt(log2Ceil(nmaxpcs).W))
     val iemdata       = Input(Vec(width, SInt(iembw.W)))
     val iemdataverify = Output(Vec(width, SInt(iembw.W)))
   })
 
   io.out.valid := false.B
-  io.out.bits := 0.U.asTypeOf(Vec(npcs, SInt(redbw.W)))
+  io.out.bits := 0.U.asTypeOf(Vec(nmaxpcs, SInt(redbw.W)))
   io.iemdataverify := 0.U.asTypeOf(Vec(width, SInt(iembw.W)))
 }
 
